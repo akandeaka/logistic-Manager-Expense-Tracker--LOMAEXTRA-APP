bd86f9aa4c7e5cf284523c5e9d0eddf3f7bd6f51
AKINPELU OLUBUNMI AKANDE <akande.akinpelu913@gmail.com>
2026-09-20 20:33:52 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'logistics-v1';
+const ASSETS = [
+  './', './index.html', './manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js'
+];
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
